b6298fc0ae3af3e555f0d9668b9a99ff96dfc101
SwarnimBhatt96 <swarnimbhatt1996@gmail.com>
2024-03-27 00:09:10 +0530

    Added basic auth flow using mongo db

--- node-backend/index.js
+++ node-backend/index.js
@@ -1,11 +1,52 @@
 const express = require('express');
 const cors = require('cors');
+const User = require('./models/User');
+const mongoose = require('mongoose');
+const jwt = require("jsonwebtoken");
+const CONSTANTS = require('./constants');
 
 const app = express();
-app.use(cors());
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(express.json());
 
-app.post('/register', (request, response)=>{
-    response.json("test okk");
+const secretSalt = "redxcftvg4b5h63rwe37tdvguhbnj";
+
+try {
+    mongoose.connect(CONSTANTS.MONGO_DB_URL);
+}
+catch (e) {
+    console.log(e)
+}
+app.post('/register', async (request, response) => {
+    const { username, password } = request.body;
+
+    try {
+        const userDoc = await User.create({ username, password });
+        response.json(userDoc);
+    }
+    catch (e) {
+        response.status(400).json(e);
+    }
+
+});
+
+app.post('/login', async (request, response) => {
+    const { username, password } = request.body;
+    const userDoc = await User.findOne({ username });
+    const isValidUserLogin = password == userDoc.password;
+    if (isValidUserLogin) {
+        try {
+            const token = jwt.sign({ username, id: userDoc._id }, secretSalt,)
+            response.cookie("token", token).json("ok");
+        }
+        catch (e) {
+            console.error(e);
+        }
+
+    }
+    else {
+        response.status(400).json("Wrong credentials");
+    }
 });
 
 app.listen(4000);
--- react-blog-app/src/pages/LoginPage.js
+++ react-blog-app/src/pages/LoginPage.js
@@ -1,10 +1,49 @@
+import { useState } from "react";
+import { Navigate } from 'react-router-dom';
+
 export default function LoginPage() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [redirect, setRedirect] = useState(false);
+
+    async function login(e) {
+        e.preventDefault();
+        const response = await fetch("http://localhost:4000/login", {
+            method: "POST",
+            body: JSON.stringify({ username, password }),
+            headers: { "Content-Type": "application/json" },
+            credentials: "include"
+        });
+        if (response.status === 200) {
+            alert("login successfull!");
+            setRedirect(true);
+        }
+        else {
+            alert("login failed");
+        }
+    }
+
     return (
-        <form className="login">
-            <h1>Login</h1>
-            <input type="email" placeholder="email" />
-            <input type="password" placeholder="password" />
-            <button>Login</button>
-        </form>
+        <>
+            {redirect ? (<Navigate to={'/'} />)
+                :
+                (
+                    <form className="login" onSubmit={login}>
+                        <h1>Login</h1>
+                        <input
+                            type="email"
+                            placeholder="email"
+                            value={username}
+                            onChange={event => setUsername(event.target.value)} />
+                        <input
+                            type="password"
+                            placeholder="password"
+                            value={password}
+                            onChange={event => setPassword(event.target.value)} />
+                        <button>Login</button>
+                    </form>
+                )}
+        </>
+
     )
 }
